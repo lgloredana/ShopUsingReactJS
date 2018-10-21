9e9b675510b47ebac869e92a1e5ea715419d8f7f
lgiulian <iuliancl2000@yahoo.com>
2018-10-21 09:05:37 +0300

    create react component for add button

--- components/add_item_button.js
+++ components/add_item_button.js
@@ -1,5 +1,26 @@
-//some code goes here
 
+'use strict';
+
+const e = React.createElement;
+
+class AddItemButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { clicked: false };
+  }
+
+  render() {
+    if (this.state.clicked) {
+      return 'You clicked add button.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ clicked: true }) },
+      '+ Add Product'
+    );
+  }
+}
 
 const domContainer = document.querySelector('#add_item_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(e(AddItemButton), domContainer);
