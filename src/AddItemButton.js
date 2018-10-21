28b6915e610f5a5e9b7159be74ad8767d1ae012b
lgiulian <iuliancl2000@yahoo.com>
2018-10-21 15:18:19 +0300

    update start page to show the add products button

---
+++ src/AddItemButton.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class AddItemButton extends Component {
+
+  render() {
+    return (
+      <div>
+        <button>+ Add Products</button>
+      </div>
+    );
+  }
+}
+
+export default AddItemButton
