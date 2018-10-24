93ec85ab59a283a42701129dc00133a65c2fc28d
lgloredana <elia_earth@yahoo.com>
2018-10-24 13:44:18 +0300

    add some simple unit tests

---
+++ src/components/AddItemButton.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AddItemButton from './AddItemButton';
+
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<AddItemButton />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
