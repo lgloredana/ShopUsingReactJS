20d635cb84ab1b3cfb9114a6f98cdeb8fa39fec7
lgloredana <elia_earth@yahoo.com>
2018-10-30 14:12:07 +0200

    update css to have a nicer look

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -31,19 +31,19 @@ class ProductItem extends Component{
         let buttons;
         if(this.props.showCompareBtn){
             buttons=<div>
-                        <button name="btnEdit" className="btnDownLeft" 
+                        <button name="btnEdit" className="btnDownLeft btn btn-success" 
                                 value={productName + "/" + productPrice} 
                                 onClick={this.onEdit}>Edit</button>
-                        <button name="btnCompare" className="btnDownRight" 
+                        <button name="btnCompare" className="btnDownRight btn btn-warning" 
                                 value={productName + "/" + productPrice} 
                                 onClick={this.onCompare}>Compare</button>
                     </div>
         } else {
             buttons=<div>
-                        <button name="btnEdit" className="btnDownLeft" 
+                        <button name="btnEdit" className="btnDownLeft btn btn-success" 
                                 value={productName + "/" + productPrice + '/' + showCompareBtn } 
                                 onClick={this.onEdit}>Edit</button>
-                        <button name="btnRemove" className="btnUpRight" 
+                        <button name="btnRemove" className="btnUpRight btn btn-danger" 
                                 value={productName} 
                                 onClick={this.onRemove}>Remove</button>
                     </div>
@@ -53,10 +53,9 @@ class ProductItem extends Component{
                 <img src={productImg} alt="no-img-product"/>
                 {buttons}
             </div>
-            <p></p>
             <div className="boxDetails">
                 <div className="itemDetailsLeft">{productName}</div>
-                <div className="itemDetailsRight">{productPrice}</div> 
+                <div className="itemDetailsRight">${productPrice}</div> 
             </div>
         </div> );
     };
